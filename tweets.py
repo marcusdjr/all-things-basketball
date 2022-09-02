ff3790daf1b8a671239ded71250a00d9a6946ebc
TatoSec <ivang.fonsecafrias@gmail.com>
2022-09-02 19:11:40 -0400

    Functionality Update

--- tweets.py
+++ tweets.py
@@ -1,15 +1,21 @@
+from operator import contains
 import snscrape.modules.twitter as sntwitter
+from PIL import Image
+import numpy as np
 import pandas as pd
 import pandas as pd
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import sys, os
+from os import path
 os.chdir(sys.path[0])
 
 #twitter filter
 query = "(#Nbatrades)"
-tweets = []
+tweets = ["Trade","Trades","trade","trades","Blockbuster Trade","blockbuster trade","Blockbuster trade","blockbuster Trade",
+          "Trading","Deal","Free Agent", "FA", "Sign", "Signing","sign", "Signing"]
 limit = 3000
+
 
 #for loop for printing content
 for tweet in sntwitter.TwitterHashtagScraper(query).get_items():
@@ -30,16 +36,29 @@ df.to_csv('tweets.csv')
 text = open('tweets.csv', mode='r', encoding='utf-8').read()
 
 #STOPWORDS
-stopwords = ('stopwords.txt')
+nba_logo = np.array(Image.open(path.join( "NBA-icon.png")))
+stopwords = set(STOPWORDS)
+stopwords.add("https")
 
-wc = WordCloud(
-    background_color='white',
-    stopwords=stopwords,
-    height = 600,
-    width= 400
-)
+wc = WordCloud(background_color="white", max_words=2000, mask=nba_logo,
+               stopwords=stopwords, max_font_size=40, random_state=42)
+
 
 wc.generate(text)
+
+#Adding color to the image based off images original color
+image_colors = ImageColorGenerator(nba_logo)
+
+# show
+fig, axes = plt.subplots(1, 3)
+axes[0].imshow(wc, interpolation="bilinear")
+# recolor wordcloud and show
+# we could also give color_func=image_colors directly in the constructor
+axes[1].imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
+axes[2].imshow(nba_logo, cmap=plt.cm.gray, interpolation="bilinear")
+for ax in axes:
+    ax.set_axis_off()
+plt.show()
 
 #storing word cloud to file
 wc.to_file('wordcloud_output.png')
