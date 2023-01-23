a90b58d3bac29e5d15b5b3854d800b08b519418c
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2023-01-22 21:42:32 -0500

    ploomber

---
+++ nba_player_performance/pipeline/scripts/get.py
@@ -0,0 +1,71 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# Add description here
+#
+# *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
+
+
+# %%
+# Uncomment the next two lines to enable auto reloading for imported modules
+# # %load_ext autoreload
+# # %autoreload 2
+# For more info, see:
+# https://docs.ploomber.io/en/latest/user-guide/faq_index.html#auto-reloading-code-in-jupyter
+
+# %% tags=["parameters"]
+# If this task has dependencies, list them them here
+# (e.g. upstream = ['some_task']), otherwise leave as None.
+upstream = None
+
+# This is a placeholder, leave it as None
+product = None
+
+
+# %%
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# %%
+# NBA season we will be analyzing
+year = 2022
+# URL page we will scraping (see image above)
+url = "https://www.basketball-reference.com/leagues/NBA_2022_per_game.html".format(year)
+# this is the HTML from the given URL
+html = urlopen(url)
+soup = BeautifulSoup(html)
+
+# %%
+# use findALL() to get the column headers
+soup.findAll('tr', limit=2)
+# use getText()to extract the text we need into a list
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+# exclude the first column because we will not need the ranking order from Basketball Reference for the analysis
+headers = headers[1:]
+headers
+# avoid the first header row
+rows = soup.findAll('tr')[1:]
+player_stats = [[td.getText() for td in rows[i].findAll('td')]
+            for i in range(len(rows))]
+
+# %%
+stats = pd.DataFrame(player_stats, columns=["Player Name", "Position","Age","Team","g","GS","MP","FG","FGA","FG%","3P","3PA","3P%","2P","2PA","2P%","eFG%","FT","FTA","FT%","ORB","DRB","TRB","AST","STL","BLK","TOV","PF","PTS"])
+#Printing to CSV
+stats.to_csv("player_stats.csv", index=False)
+print("Data saved to player_stats.csv")
+stats.head(20)
+
+# %%
