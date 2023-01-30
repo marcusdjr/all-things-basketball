64e0221ead49eb0c479bf96bfd2dd7fa09c987b9
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2023-01-29 22:23:33 -0500

    working on getting more data

--- nba_player_performance/pipeline/scripts/get.py
+++ nba_player_performance/pipeline/scripts/get.py
@@ -38,6 +38,7 @@ product = None
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
 
 # %%
 # NBA season we will be analyzing
@@ -166,35 +167,28 @@ stats_95.head(20)
 # %%
 #Play by play nba stats
 
-
-
-# %%
-# NBA season we will be analyzing
-year = 2023
-# URL page we will scraping (see image above)
-url = "https://www.nba.com/stats/lineups/traditional?slug=traditional&ShotClockRange=&Period=4&PerMode=PerPlay".format(year)
-# this is the HTML from the given URL
-html = urlopen(url)
-soup = BeautifulSoup(html)
+season_id = '2022-23'
+per_mode = 'Totals'
+player_info_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode='+per_mode+'&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season='+season_id+'-23&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight='
 
 # %%
-# use findALL() to get the column headers
-soup.findAll('tr', limit=2)
-# use getText()to extract the text we need into a list
-headers = [td.getText() for td in soup.findAll('tr', limit=2)]
-# exclude the first column because we will not need the ranking order from Basketball Reference for the analysis
-##headers = headers[1:]
-##headers
-# avoid the first header row
-##rows = soup.findAll('tr')[1:]
-play_by_play_23 = [[td.getText() for td in rows[i].findAll('td')]
-            for i in range(len(rows))]
+headers  = {
+    'Connection': 'keep-alive',
+    'Accept': 'application/json, text/plain, */*',
+    'x-nba-stats-token': 'true',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+    'x-nba-stats-origin': 'stats',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Referer': 'https://stats.nba.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
 
 # %%
-play_by_play_23 = pd.DataFrame(play_by_play_23, columns=["Player Name", "Position","Age","Team","G","GS","MP"])
-#Printing to CSV
-play_by_play_23.to_csv("play_by_play_23.csv", index=False)
-print("Data saved to play_by_play_23.csv")
-play_by_play_23.head()
+requests.get(url=player_info_url, headers=headers).json()
+
+# %%
+player_info = response['resultsSet'][0]['rowSet']
 
 # %%
