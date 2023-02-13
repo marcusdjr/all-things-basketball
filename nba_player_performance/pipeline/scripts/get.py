5cc87e02c7c6929f89bed42071c109aecc91b7f6
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2023-02-12 22:58:12 -0500

    getting play by play data

--- nba_player_performance/pipeline/scripts/get.py
+++ nba_player_performance/pipeline/scripts/get.py
@@ -165,30 +165,31 @@ print("Data saved to player_stats_95.csv")
 stats_95.head(20)
 
 # %%
-#Play by play nba stats
-
-season_id = '2022-23'
-per_mode = 'Totals'
-player_info_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2022-23&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight='
-
-# %%
-headers  = {
-    'Connection': 'keep-alive',
-    'Accept': 'application/json, text/plain, */*',
-    'x-nba-stats-token': 'true',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-    'x-nba-stats-origin': 'stats',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'cors',
-    'Referer': 'https://stats.nba.com/',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-}
+#Season of 1995
+# NBA season we will be analyzing
+year = 2022
+# URL page we will scraping (see image above)
+url = "https://www.basketball-reference.com/leagues/NBA_2022_play-by-play.html".format(year)
+# this is the HTML from the given URL
+html = urlopen(url)
+soup = BeautifulSoup(html)
 
 # %%
-response = requests.get(url=player_info_url, headers=headers).json
+# use findALL() to get the column headers
+soup.findAll('tr', limit=2)
+# use getText()to extract the text we need into a list
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+# exclude the first column because we will not need the ranking order from Basketball Reference for the analysis
+headers = headers[1:]
+headers
+# avoid the first header row
+rows = soup.findAll('tr')[1:]
+player_stats_95 = [[td.getText() for td in rows[i].findAll('td')]
+            for i in range(len(rows))]
 
 # %%
-player_info = response['resultSets']['0']['rowSet']
-
-# %%
+stats_95 = pd.DataFrame(player_stats_95, columns=["Player", "Position","Age","Team","G","MP","PGP","SGP","SFP","PFP","CP","OnCourt","On-Off","BadPass","LostBall","Shoot","Off","Shoot","ffD","PGA","And1","Bkd"])
+#Printing to CSV
+stats_95.to_csv("player_stats_95.csv", index=False)
+print("Data saved to player_stats_95.csv")
+stats_95.head(20)
